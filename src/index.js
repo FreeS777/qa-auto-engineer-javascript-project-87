9de85f1f87bf998fa4b904aa18b38f4ec1575d0b
Artem S <frees.comp@gmail.com>
2024-08-08 09:16:57 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -22,24 +22,25 @@ const isAddedKey = (key, obj1, obj2) => !_.has(obj1, key) && _.has(obj2, key);
 const isDeletedKey = (key, obj1, obj2) => _.has(obj1, key) && !_.has(obj2, key);
 const hasSameValue = (key, obj1, obj2) => obj1[key] === obj2[key];
 
+const formatLine = (symbol, key, value) => `  ${symbol} ${key} : ${value}`;
+
 export default (filename1, filename2) => {
   const obj1 = getData(filename1);
   const obj2 = getData(filename2);
   const allkeys = _.union(_.keys(obj1), _.keys(obj2)).sort();
 
   const result = allkeys.map((key) => {
-    if (isSharedKey(key, obj1, obj2) && hasSameValue(key, obj1, obj2)) {
-      return `  ${symbols.same} ${key} : ${obj1[key]}`
-    } 
-    if (isSharedKey(key, obj1, obj2) && !hasSameValue(key, obj1, obj2)) {
-      return `  ${symbols.del} ${key} : ${obj1[key]}\n  ${symbols.add} ${key} : ${obj2[key]}`;
+    if (isSharedKey(key, obj1, obj2)) {
+      return hasSameValue(key, obj1, obj2)
+        ? formatLine(symbols.same, key, obj1[key])
+        : `${formatLine(symbols.del, key, obj1[key])}\n${formatLine(symbols.add, key, obj2[key])}`;
     }
     if (isAddedKey(key, obj1, obj2)) {
-      return `  ${symbols.add} ${key} : ${obj2[key]}`;
+      return formatLine(symbols.add, key, obj2[key]);
     }
     if (isDeletedKey(key, obj1, obj2)) {
-      return `  ${symbols.del} ${key} : ${obj1[key]}`;
-    }    
+      return formatLine(symbols.del, key, obj1[key]);
+    }
   });
 
   return `{\n${result.join("\n")} \n}`;
