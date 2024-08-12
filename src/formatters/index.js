d0ff4ab6a740f8d5ccce2904c65390836bbe7b36
Artem Smirnov <frees.comp@gmail.com>
2024-08-12 19:04:33 +0300

    fix formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,12 @@
 import plainFormat from './plain.js';
-import defaultFormat from './default.js';
+import stylishFormat from './stylish.js';
 
 export default (format, structure) => {
   switch (format) {
     case 'plain':
       return plainFormat(structure);
-    case 'default':
-      return defaultFormat(structure);
+    case 'stylish':
+      return stylishFormat(structure);
     case 'json':
       return JSON.stringify(structure, null, 2);
     default:
