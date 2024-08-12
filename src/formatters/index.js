b18879865b39b9539244d63ca8681fcaac6b5b6b
Artem Smirnov <frees.comp@gmail.com>
2024-08-12 18:57:32 +0300

    fix formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,15 @@
 import plainFormat from './plain.js';
 import defaultFormat from './default.js';
 
-const jsonFormat = (structure) => JSON.stringify(structure, null, 2);
-const formatHandlers = {
-  plain: plainFormat,
-  default: defaultFormat,
-  json: jsonFormat,
+export default (format, structure) => {
+  switch (format) {
+    case 'plain':
+      return plainFormat(structure);
+    case 'default':
+      return defaultFormat(structure);
+    case 'json':
+      return JSON.stringify(structure, null, 2);
+    default:
+      throw new Error(`Unknown format '${format}'.`);
+  }
 };
-
-export default (format, structure) => formatHandlers[format](structure);
